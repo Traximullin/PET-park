2cbf274f714262a571fc1a946e8581a892a8086e
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-04-30 21:35:30 +0300

    modal-logic

--- src/shared/ui/Modal/index.tsx
+++ src/shared/ui/Modal/index.tsx
@@ -3,12 +3,25 @@ import "./index.scss"
 import { type IModal } from "./interface"
 
 const Modal: FC<IModal> = (props) => {
-    const { children, ...othersProps } = props
+    const { active, setActive, children, ...othersProps } = props
+
+    const currentClasses = ["modal"]
+
+    if (active) currentClasses.push("modal_active")
+
+    const handleCloseClick = (): void => { setActive(prev => !prev) }
 
     return (
-        <div className="modal" {...othersProps}>
+        <div
+            className={currentClasses.join(" ")}
+            {...othersProps}
+        >
             <div className="modal__content">
-                123
+                <div className="modal__close" onClick={handleCloseClick}>
+                    <span className="modal__close-icon"/>
+                    123
+                </div>
+                Привет
             </div>
         </div>
     )
