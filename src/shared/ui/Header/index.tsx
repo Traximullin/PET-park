626bf798ef763ad23a77e1e0c5f5679cd5b0414d
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-05-01 18:07:18 +0300

    header

---
+++ src/shared/ui/Header/index.tsx
@@ -0,0 +1,14 @@
+import { type FC } from "react"
+import { Logo } from ".."
+import "./index.scss"
+
+const Header: FC = () => {
+    return (
+        <header className="header">
+            <Logo />
+
+        </header>
+    )
+}
+
+export default Header
