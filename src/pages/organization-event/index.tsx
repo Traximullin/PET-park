7bc3c6d09928e6252d57354e61936d2de200b664
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-05-08 22:53:30 +0300

    base-layout

---
+++ src/pages/organization-event/index.tsx
@@ -0,0 +1,11 @@
+import { type FC } from "react"
+
+const OrganizationEventPage: FC = () => {
+    return (
+        <div>
+            1
+        </div>
+    )
+}
+
+export default OrganizationEventPage
