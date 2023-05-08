8dd9d13bef98226ed5063c9bd45b96cf7f1cf4f3
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-05-08 23:16:04 +0300

    page organization event

--- src/pages/organization-event/index.tsx
+++ src/pages/organization-event/index.tsx
@@ -1,10 +1,22 @@
 import { type FC } from "react"
+import { BaseLayout, Space } from "shared/layouts"
+import { Box, Calendar, Tag } from "shared/ui"
 
 const OrganizationEventPage: FC = () => {
     return (
-        <div>
-            1
-        </div>
+        <BaseLayout>
+            <Space gap="14px">
+                <Box>
+                    <Calendar />
+                </Box>
+                <Box>
+                    <Tag name="time">10:00</Tag>
+                </Box>
+                <Box>
+
+                </Box>
+            </Space>
+        </BaseLayout>
     )
 }
 
